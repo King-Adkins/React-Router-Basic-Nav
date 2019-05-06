2bd8a69ea8ff602b23e550465bed3b0312f1ed83
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-05-06 18:33:38 -0400

    mvp hit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { router } from 'sw-toolbox';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
